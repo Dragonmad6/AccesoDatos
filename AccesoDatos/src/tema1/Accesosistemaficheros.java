bf22a7c1e7c4b6e6975decf311b0b5283a6c6192
Carlos <ccalvocalzado@gmail.com>
2021-09-16 19:33:39 +0200

    NUevo
    nuevo

---
+++ AccesoDatos/src/tema1/Accesosistemaficheros.java
@@ -0,0 +1,25 @@
+package tema1;
+
+import java.io.File;
+
+public class Accesosistemaficheros {
+	public static void main(String[] args) {
+		File carpeta1 = new File("\\");
+		//Give you a list of the carpets and archives.
+		File[] listaCarpetas = carpeta1.listFiles();
+		
+		System.out.println(carpeta1.getName());
+		
+		for(int i=0; i<listaCarpetas.length; i++) {
+			if(listaCarpetas[i].isDirectory()) {
+				System.out.println("--");
+			}
+			if(listaCarpetas[i].getName().endsWith("txt")) {
+				System.out.println("-txt-");
+			}
+			System.out.println(listaCarpetas[i].getName());
+		}
+	}
+
+	
+}
