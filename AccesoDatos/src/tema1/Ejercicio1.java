82124c6abac6c7330b4515e8e163343064989d75
Carlos <ccalvocalzado@gmail.com>
2021-09-17 16:05:07 +0200

    En clase 17/09

--- AccesoDatos/src/tema1/Ejercicio1.java
+++ AccesoDatos/src/tema1/Ejercicio1.java
@@ -1,34 +1,38 @@
 package tema1;
 
 import java.io.File;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Ejercicio1 {
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		String nombre;
-				
+		boolean bucle = false;
 		//Name Route
+		//		C:\Users\Carlosc\Desktop\Pruebas01.txt
 		do {
-			
+
 			System.out.println("Escribe una ruta:\n");
 			nombre = sc.nextLine();
-			
+
 			File carpeta1 = new File(nombre);
-			
-			if(carpeta1.length()<5) {
-				System.out.println("No supera los caracteres requeridos.");
-			}
+			System.out.println(carpeta1.getName());
+
+			//System.out.println(carpeta1.getName());
 			if(carpeta1.isDirectory()) {
 				System.out.println("Es un directorio.");
-			}else if(carpeta1.isDirectory()){
-				System.out.println("Es un directorio.");
+			}			
+			if(carpeta1.getName().endsWith("txt")) {
+				if((carpeta1.getName().length()-4)>5) {   
+					System.out.println("Está bien");
+					bucle = true;
+				}
 			}
-			
-			
-		}while(nombre.length()<5);
-		
+
+		}while(bucle);
+
 	}
 }
